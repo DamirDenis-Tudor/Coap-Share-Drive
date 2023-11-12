260975992e7afb0a5492a4b78b81529d28b07787
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2023-11-12 19:59:16 +0200

    feat[source/PacketManager]:Packet, TokenGen, Config has been added.

--- source/PacketManager/Packet.py
+++ source/PacketManager/Packet.py
@@ -1,30 +1,117 @@
+from bitfields import Bits
+from source.PacketManager.PacketUtils.Config import *
+from source.PacketManager.PacketUtils.TokenGen import TokenGenerator
+
+
 class Packet:
-    def __init__(self, raw_packet):
-        # Extracting the bit ranges using bitwise operations
-        self.__packet_type = raw_packet & 0b11
-        self.__token_length = (raw_packet >> 2) & 0b1111
-        self.__packet_code = ((raw_packet >> 6) & 0b111, (raw_packet >> 7) & 0b11111)
-        self.__packet_id = (raw_packet >> 16) & 0xFFFF
-        self.__token = (raw_packet >> 32 & ((1 >> self.__token_length) - 1))
-        self.__depth = (raw_packet >> (32 + self.__token_length - 1) & ((1 >> self.__token_length) - 1))
+    def __init__(self, raw_packet: int, external_ip: str):
+        bits_packet = Bits(raw_packet)
+        self.extern_ip = external_ip
+        self.coap_ver = format(int(bits_packet[0]), '01b')
+        self.packet_type = PacketType.get_field_name(format(int(bits_packet[1:3]), '02b'))
+        self.token_length = int(bits_packet[3:7])
+        self.packet_code = PacketCode.get_field_name(format(int(bits_packet[7:15]), '08b'))
+        self.packet_id = int(bits_packet[15:31])
 
-    def encode(self):
-        # Encode the attributes back into raw bits
-        encoded_packet = 0
+        index = self.token_length + 31
+        self.token = int(bits_packet[31:index])
+        self.packet_depth = int(bits_packet[index:index + 4])
 
-        encoded_packet |= (self.__packet_type)
-        encoded_packet |= (self.__token_length << 2)
-        encoded_packet |= (self.__packet_code[0] << 6)
-        encoded_packet |= (self.__packet_code[1] << 9)
+        index += 4
+        self.multiple_packets = MultiplePackets.get_field_name(format(int(bits_packet[index:index + 1]), '01b'))
 
-        return encoded_packet
+        index += 1
+        self.payload_format = PayloadFormat.get_field_name(format(int(bits_packet[index:index + 2]), '02b'))
+
+        index += 2
+        self.payload = None
+        if self.payload_format == PayloadFormat.OPAQUE:
+            self.payload = bits_packet[index:]
+        elif self.payload_format == PayloadFormat.UINT:
+            self.payload = int(bits_packet[index:])
+        elif self.payload_format == PayloadFormat.STRING:
+            self.payload = bits_packet[index:].to_bytes().decode('utf-8')
+
+    def __str__(self):
+        return (
+            f"Packet Details \n"
+            f"  From: {self.extern_ip}\n"
+            f"  Coap Version: {self.coap_ver}\n"
+            f"  Packet Type: {self.packet_type}\n"
+            f"  Token Length: {self.token_length}\n"
+            f"  Packet Code: {self.packet_code}\n"
+            f"  Packet ID: {self.packet_id}\n"
+            f"  Token: {self.token}\n"
+            f"  Packet Depth: {self.packet_depth}\n"
+            f"  Multiple Packets: {self.multiple_packets}\n"
+            f"  Payload Format: {self.payload_format}\n"
+            f"  Payload: {self.payload}\n"
+        )
+
+    def __repr__(self):
+        return str(self)
+
+    @staticmethod
+    def encode(content: dict):
+        coap_ver = str(content.get("CoapVer"))
+        packet_type = content.get("PacketType").value
+        token_length = content.get("TokenLength")
+        packet_code = content.get("PacketCode").value
+        packet_id = format(content.get("PacketId"), '016b')
+        token = content.get("Token")
+        packet_depth = format(content.get("PacketDepth"), '04b')
+        multiple_packets = content.get("MultiplePackets").value
+        payload_format = content.get("PayloadFormat").value
+
+        payload = ''
+        try:
+            if content.get("PayloadFormat") == PayloadFormat.STRING:
+                payload = ''.join(format(ord(b), '08b') for b in str(content.get("Payload")))
+            elif content.get("PayloadFormat") == PayloadFormat.UINT:
+                payload = bin(content.get("Payload"))[2:]
+            elif content.get("PayloadFormat") == PayloadFormat.OPAQUE:
+                payload = bin(content.get("Payload"))[2:]
+        except (AttributeError, TypeError, UnicodeEncodeError) as invalid_payload:
+            print(f"Invalid payload: {invalid_payload}")
+            # Handle the exception or add additional logging here
+
+        bits_sequence = coap_ver + packet_type + token_length + packet_code + packet_id + token \
+                        + packet_depth + multiple_packets + payload_format + payload
+
+        matched_bytes = int(bits_sequence, 2).to_bytes((len(bits_sequence) + 7) // 8, byteorder='big')
+
+        return int.from_bytes(matched_bytes, byteorder='big')
 
 
-# Example usage
-raw_packet_value = 0b100101100000011111001010100000010101000101010
+for x in range(1, 30):
+    # Test the encode method
+    encoded_packet = Packet.encode({
+        "CoapVer": 1,
+        "PacketType": PacketType.CON,
+        "TokenLength": TokenGenerator.TOKEN_LENGTH,
+        "PacketCode": PacketCode.RequestCode.GET,
+        "PacketId": 0,
+        "Token": TokenGenerator.generate_token(),
+        "PacketDepth": 0,
+        "MultiplePackets": MultiplePackets.MULTIPLE,
+        "PayloadFormat": PayloadFormat.STRING,
+        "Payload": "/text.txt"
+    })
 
-packet = Packet(raw_packet_value)
+    packet = Packet(encoded_packet, "192.125.111.15")
+    print(packet)
 
-# Encoding the packet back to raw bits
-encoded_packet = packet.encode()
-print(f"Encoded Packet: {bin(encoded_packet)}")  # Print the binary representation of the encoded packet
+    encoded_packet1 = Packet.encode({
+        "CoapVer": 1,
+        "PacketType": PacketType.RST,
+        "TokenLength": TokenGenerator.TOKEN_LENGTH,
+        "PacketCode": PacketCode.RequestCode.EMPTY,
+        "PacketId": 0,
+        "Token": TokenGenerator.generate_token(),
+        "PacketDepth": 0,
+        "MultiplePackets": MultiplePackets.SINGLE,
+        "PayloadFormat": PayloadFormat.EMPTY,
+        "Payload": ""
+    })
+    packet1 = Packet(encoded_packet1, "192.125.0.15")
+    print(packet1)
---
+++ source/PacketManager/PacketUtils/Config.py
@@ -0,0 +1,88 @@
+from enum import Enum
+
+
+class TransmissionParameters(Enum):
+    """
+        MAX_TRANSMIT_WAIT = ACK_TIMEOUT * ((2 ** ( MAX_RETRANSMIT + i )) - 1) * ACK_RANDOM_FACTOR
+    """
+    ACK_TIMEOUT = 2
+    ACK_RANDOM_FACTOR = 1.5
+    MAX_RETRANSMIT = 4
+    NSTART = 1
+    DEFAULT_LEISURE = 5
+
+
+class PacketType(Enum):
+    CON = "00"
+    NON = "01"
+    ACK = "10"
+    RST = "11"
+
+    @staticmethod
+    def get_field_name(value):
+        for member in PacketType:
+            if member.value == value:
+                return member
+        return None
+
+
+class PayloadFormat(Enum):
+    EMPTY = "00"
+    OPAQUE = "01"
+    UINT = "10"
+    STRING = "11"
+
+    @staticmethod
+    def get_field_name(value):
+        for member in PayloadFormat:
+            if member.value == value:
+                return member
+        return None
+
+
+class PacketCode:
+    class RequestCode(Enum):
+        EMPTY = "00000000"
+        GET = "00000001"
+        POST = "00000010"
+        PUT = "00000011"
+        DELETE = "00000100"
+
+    class SuccessResponse(Enum):
+        CREATED = "01000001"
+        DELETED = "01000010"
+        VALID = "01000011"
+        CHANGED = "01000100"
+        CONTENT = "01000101"
+
+    class ClientErrorResponse(Enum):
+        BAD_REQUEST = "10000000"
+        UNAUTHORIZED = "10000001"
+        NOT_FOUND = "10000100"
+        METHOD_NOT_ALLOWED = "10000101"
+
+    class ServerErrorResponse(Enum):
+        INTERNAL_SERVER_ERROR = "10100000"
+        NOT_IMPLEMENTED = "10100001"
+        SERVICE_UNAVAILABLE = "10100011"
+
+    @staticmethod
+    def get_field_name(value):
+        for inner_class in [PacketCode.RequestCode, PacketCode.SuccessResponse,
+                            PacketCode.ClientErrorResponse, PacketCode.ServerErrorResponse]:
+            for member in inner_class:
+                if member.value == value:
+                    return member
+        return None
+
+
+class MultiplePackets(Enum):
+    SINGLE = "0"
+    MULTIPLE = "1"
+
+    @staticmethod
+    def get_field_name(value):
+        for member in MultiplePackets:
+            if member.value == value:
+                return member
+        return None
